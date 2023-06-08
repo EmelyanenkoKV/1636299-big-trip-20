97859dd99620fa0204edd17a822e1126acb74868
Chris <christina.emelyanenko08@gmail.com>
2023-06-08 22:42:19 +0200

    7.10. Меняй-удаляй правки

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -3,6 +3,7 @@ import {CITIES, POINT_EMPTY, WAYPOINTS} from '../const';
 import {getRefineFullDate} from '../utils/points';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import he from 'he';
 
 function createPointEditTypeTemplate(currentType) {
   return WAYPOINTS.map((type) => `
@@ -94,7 +95,7 @@ function createPointEditTemplate({state, destinations, offers}) {
             ${type}
             </label>
             <input class="event__input  event__input--destination" id="event-destination-1"
-            type="text" name="event-destination" value="${isDestinationName}" list="destination-list-1">
+            type="text" name="event-destination" value="${he.encode(isDestinationName)}" list="destination-list-1">
             <datalist id="destination-list-1"/>
               ${citiesTemplate}
             </datalist>
@@ -103,11 +104,11 @@ function createPointEditTemplate({state, destinations, offers}) {
           <div class="event__field-group  event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
             <input class="event__input  event__input--time" id="event-start-time-1" type="text"
-            name="event-start-time" value="${dateFullFrom}">
+            name="event-start-time" value="${he.encode(dateFullFrom)}">
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
             <input class="event__input  event__input--time" id="event-end-time-1" type="text"
-            name="event-end-time" value="${dateFullTo}">
+            name="event-end-time" value="${he.encode(dateFullTo)}">
           </div>
 
           <div class="event__field-group  event__field-group--price">
@@ -116,7 +117,7 @@ function createPointEditTemplate({state, destinations, offers}) {
               &euro;
             </label>
             <input class="event__input  event__input--price" id="event-price-1"
-            type="text" name="event-price" value="${basePrice}"/>
+            type="number" min="0" name="event-price" value="${basePrice}"/>
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
